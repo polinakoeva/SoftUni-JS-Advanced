5dd7fea717e7906453e372dc4ce21f844cd104bf
Polina Koeva <polina.koeva@gmail.com>
2022-05-09 23:02:17 +0200

    Delete Functions directory

--- Functions/01.smallestOfThreeNumbers.js
+++
@@ -1,5 +0,0 @@
-function getSmallestNum(firstNum, secondNum, thirdNum) {
-  let smallestNum = Math.min(firstNum, secondNum, thirdNum);
-  return smallestNum;
-}
-console.log(getSmallestNum(2, 5, 3));
